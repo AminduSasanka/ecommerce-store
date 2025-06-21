82a2f5dc7415b1a2bbf2f154c57e7b2a9ebcc86f
reservedchery <reservedchery@gmail.com>
2025-06-21 18:04:23 +0530

    added role repository

---
+++ store/src/main/java/com/ecommerece/store/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.ecommerece.store.repository;
+
+import com.ecommerece.store.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
