ebcac7f06a2569139bd2f40c301e4da12570f067
reservedchery <reservedchery@gmail.com>
2025-06-21 15:48:11 +0530

    added role mappings

--- store/src/main/java/com/ecommerece/store/model/User.java
+++ store/src/main/java/com/ecommerece/store/model/User.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 
 @Getter
@@ -20,10 +22,17 @@ public class User {
     private String email;
     private String phone;
     private String address;
+    private String password;
 
     @OneToOne(mappedBy ="user", cascade = CascadeType.ALL, orphanRemoval = true)
     private Cart cart;
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Order> orders;
+
+    @ManyToMany(fetch = FetchType.EAGER,
+            cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+    private Collection<Role> roles = new HashSet<>();
 }
