39fc984c1f7ebc1d762eefaae9ca5fc5391d1e23
reservedchery <reservedchery@gmail.com>
2025-06-21 20:56:16 +0530

    added login request

---
+++ store/src/main/java/com/ecommerece/store/controller/AuthController.java
@@ -0,0 +1,47 @@
+package com.ecommerece.store.controller;
+
+import com.ecommerece.store.request.LoginRequest;
+import com.ecommerece.store.response.ApiResponse;
+import com.ecommerece.store.response.JwtResponse;
+import com.ecommerece.store.security.jwt.JwtUtils;
+import com.ecommerece.store.security.user.ShopUserDetails;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtils jwtUtils;
+
+    @PostMapping("/login")
+    public ResponseEntity<ApiResponse> login(@Valid @RequestBody LoginRequest loginRequest) {
+        try {
+            Authentication authentication = authenticationManager
+                    .authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword()));
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            String jwt = jwtUtils.generateTokenForUser(authentication);
+            ShopUserDetails userDetails = (ShopUserDetails) authentication.getPrincipal();
+            JwtResponse jwtResponse = new JwtResponse(userDetails.getId(), jwt);
+
+            return ResponseEntity.ok(new ApiResponse("login success", jwtResponse));
+        } catch (AuthenticationException e) {
+            return ResponseEntity.status(UNAUTHORIZED).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+}
