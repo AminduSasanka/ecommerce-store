e220ef266e7aa3dfb39d8c6e670658a7c85433b0
reservedchery <reservedchery@gmail.com>
2025-06-21 15:48:31 +0530

    added spring security logics

---
+++ store/src/main/java/com/ecommerece/store/security/user/ShopUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.ecommerece.store.security.user;
+
+import com.ecommerece.store.model.User;
+import com.ecommerece.store.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ShopUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = Optional.ofNullable(userRepository.findByEmail(email))
+                .orElseThrow(() -> new UsernameNotFoundException(email));
+
+        return ShopUserDetails.buildUserDetails(user);
+    }
+}
